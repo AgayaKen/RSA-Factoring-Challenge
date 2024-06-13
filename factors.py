6836bf4fa9c3d48aef215297bfae7a575fcb2f50
AgayaKen <kennedyagaya@gmail.com>
2024-06-13 08:04:27 -0700

    Factoring challenge

--- factors.py
+++ factors.py
@@ -1,36 +1,22 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+#import pdb; pdb.set_trace()
+import math
 import sys
 
-def factorize(n):
-    factors = []
-    for i in range(2, n // 2 + 1):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-    if n > 1:
-        factors.append(n)
-    return factors
+filename = sys.argv[1]
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: {} <file>".format(sys.argv[0]))
-        sys.exit(1)
-
-    input_file = sys.argv[1]
-
-    try:
-        with open(input_file, 'r') as f:
-            for line in f:
-                number = int(line.strip())
-                factors = factorize(number)
-                if len(factors) >= 2:
-                    p, q = factors[0], factors[1]
-                    print("{}={}*{}".format(number, p, q))
-    except FileNotFoundError:
-        print("File not found:", input_file)
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
-
+with open(filename, 'r', encoding='utf-8') as f:
+    a = f.read()
+    a_l = a.count("\n")
+lineas = 0
+b = 0
+with open(filename, 'r', encoding='utf-8') as f:
+    while lineas < a_l:
+        n1 = int (f.readline())
+        lineas += 1
+        a = 2
+        while a < n1//2:
+            if math.fmod(n1, a) == 0:
+                print ("{:d} = {:d} * {:d}".format(n1, a, int(n1/a), end=""))
+                a = n1
+            a += 1
